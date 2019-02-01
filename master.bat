280daccbce669399c8870a99ff9b4501ed660372
ketaki2004pune <42198235+ketaki2004pune@users.noreply.github.com>
2019-02-01 15:14:45 +0530

    Update master.bat

--- master.bat
+++ master.bat
@@ -1,6 +1,4 @@
 @echo off
-cd Debug
-echo %cd%
+cd C:\Users\Administrator\Desktop\Bamboo\BambooRepository-master\Debug
 start Master.exe -ts=Master.rxtst -testrail -truser=hema.patil@cytel.com -trpass=abc123** -trrunname="Master_TestRun"
-
 exit
